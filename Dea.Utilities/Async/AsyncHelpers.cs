a2d6b0917b5f04ed611eb670ff91f7bb9ec6a857
Dale Alleshouse <dalealleshouse@gmail.com>
2014-09-24 21:13:47 -0400

    Added Async helpoers

---
+++ Dea.Utilities/Async/AsyncHelpers.cs
@@ -0,0 +1,83 @@
+﻿//  --------------------------------
+//  <copyright file="AsyncHelpers.cs">
+//      Copyright (c) 2014 All rights reserved.
+//  </copyright>
+//  <author>Alleshouse, Dale</author>
+//  <date>09/24/2014</date>
+//  ---------------------------------
+
+namespace Dea.Utilities.Async
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class AsyncHelpers
+    {
+        /**************************************************************************************************
+         * WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING 
+         * 
+         * Whenever possible, async methods should run asynchronously. These should ONLY be used when
+         * ABSOLUTELY necessary! This is WILDLY inefficient.
+         * 
+         * WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING 
+        **************************************************************************************************/
+
+        public static void RunSync(Func<Task> task)
+        {
+            var oldContext = SynchronizationContext.Current;
+            var synch = new ExclusiveSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(synch);
+            synch.Post(
+                async _ =>
+                    {
+                        try
+                        {
+                            await task();
+                        }
+                        catch (Exception e)
+                        {
+                            synch.InnerException = e;
+                            throw;
+                        }
+                        finally
+                        {
+                            synch.EndMessageLoop();
+                        }
+                    },
+                null);
+            synch.BeginMessageLoop();
+
+            SynchronizationContext.SetSynchronizationContext(oldContext);
+        }
+
+        public static T RunSync<T>(Func<Task<T>> task)
+        {
+            var oldContext = SynchronizationContext.Current;
+            var synch = new ExclusiveSynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(synch);
+            T ret = default(T);
+            synch.Post(
+                async _ =>
+                    {
+                        try
+                        {
+                            ret = await task();
+                        }
+                        catch (Exception e)
+                        {
+                            synch.InnerException = e;
+                            throw;
+                        }
+                        finally
+                        {
+                            synch.EndMessageLoop();
+                        }
+                    },
+                null);
+            synch.BeginMessageLoop();
+            SynchronizationContext.SetSynchronizationContext(oldContext);
+            return ret;
+        }
+    }
+}
---
+++ Dea.Utilities/Async/ExclusiveSynchronizationContext.cs
@@ -0,0 +1,102 @@
+﻿//  --------------------------------
+//  <copyright file="ExclusiveSynchronizationContext.cs">
+//      Copyright (c) 2014 All rights reserved.
+//  </copyright>
+//  <author>Alleshouse, Dale</author>
+//  <date>09/24/2014</date>
+//  ---------------------------------
+
+namespace Dea.Utilities.Async
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class ExclusiveSynchronizationContext : SynchronizationContext, IDisposable
+    {
+        private readonly Queue<Tuple<SendOrPostCallback, object>> _items = new Queue<Tuple<SendOrPostCallback, object>>();
+
+        private readonly AutoResetEvent _workItemsWaiting = new AutoResetEvent(false);
+
+        private bool _done;
+
+        public Exception InnerException { get; set; }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~ExclusiveSynchronizationContext()
+        {
+            this.Dispose(false);
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            throw new NotSupportedException("We cannot send to our same thread");
+        }
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            lock (this._items)
+            {
+                this._items.Enqueue(Tuple.Create(d, state));
+            }
+
+            this._workItemsWaiting.Set();
+        }
+
+        public void EndMessageLoop()
+        {
+            this.Post(_ => this._done = true, null);
+        }
+
+        public void BeginMessageLoop()
+        {
+            while (!this._done)
+            {
+                Tuple<SendOrPostCallback, object> task = null;
+                lock (this._items)
+                {
+                    if (this._items.Count > 0)
+                    {
+                        task = this._items.Dequeue();
+                    }
+                }
+
+                if (task != null)
+                {
+                    task.Item1(task.Item2);
+
+                    // the method threw an exception
+                    if (this.InnerException != null)
+                    {
+                        throw new AggregateException("AsyncHelpers.Run method threw an exception.", this.InnerException);
+                    }
+                }
+                else
+                {
+                    this._workItemsWaiting.WaitOne();
+                }
+            }
+        }
+
+        public override SynchronizationContext CreateCopy()
+        {
+            return this;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this._workItemsWaiting != null)
+                {
+                    this._workItemsWaiting.Dispose();
+                }
+            }
+        }
+    }
+}
